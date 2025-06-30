39d60aa958fc00e7b504855940b1405764aa4fa3
David <d284liu@uwaterloo.ca>
2025-06-30 16:54:54 -0400

    connected frontend to backend

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-import asyncio
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
-from llama_index.core.agent.workflow import FunctionAgent
 from llama_index.llms.openai import OpenAI
-from llama_index.core.workflow import Context
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+from llama_index.core.memory import ChatMemoryBuffer
+from llama_index.core.tools import QueryEngineTool, ToolMetadata
+from llama_index.agent.openai import OpenAIAgent
 from dotenv import load_dotenv
 
 # TODO: 
@@ -16,39 +16,45 @@ documents = SimpleDirectoryReader(input_dir="./shopify_mock_dataset").load_data(
 index = VectorStoreIndex.from_documents(documents)
 query_engine = index.as_query_engine(verbose=False)
 
-# text-based recommendation function
-def recommend(query: str) -> str:
-    response = query_engine.query(query)
-    return str(response)
+# text-based recommendation
+tool = QueryEngineTool(
+    query_engine=query_engine,
+    metadata=ToolMetadata(
+        name="ProductQA",
+        description="Answer questions about product details or recommendations."
+    ),
+)
 
-tools=[
-    recommend
-]
+
+# to memorize previous discussions; token limit is to reduce strain on OpenAI
+memory = ChatMemoryBuffer.from_defaults(token_limit=1500)
 
 # Create an agent workflow
-agent = FunctionAgent(
-    tools=tools,
+agent = OpenAIAgent.from_tools(
+    tools=[tool],
+    memory=memory,
     llm=OpenAI(model="gpt-4o-mini"),
+    verbose=False,
     system_prompt="""You are an AI Agent for an e-commerce website.
     This website only sells the products in this VectorStoreIndex.
     You can have general conversations with the user (while remembering context) and recommend products based on text prompts or images.
     Responses should be 5 sentences or less.""",
 )
-# to memorize previous discussions
-ctx = Context(agent)
 
-async def respond():
-    # Run the agent
-    while True:
-        query = input("Say something to ChatGPT: ")
+def get_response(text):
+    response = agent.chat(text)
+    return response
 
-        if query in ["bye", "goodbye"]:
-            break
+# DEBUG
+# def main():
+#     while True:
+#         query = input("[DEBUG] Say something to ChatGPT: ")
 
-        response = await agent.run(query, ctx=ctx)
-        print(str(response))
+#         if query in ["bye", "goodbye"]:
+#             break
 
+#         response = agent.chat(query)
+#         print(response)
 
-# Run the agent
-if __name__ == "__main__":
-    asyncio.run(respond())
+# if __name__ == "__main__":
+#     main()
--- frontend.py
+++ frontend.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import app
 
 st.title("Mock Commercial Website AI Agent")
 
@@ -8,23 +9,34 @@ if "messages" not in st.session_state:
 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+        match message["role"]:
+
+            case "assistant":
+                st.markdown(message["content"])
+
+            case "user":
+                st.markdown(message["content"].text)
+                if message["content"].files:
+                    st.image(message["content"].files[0])
 
 # chat input
 if prompt := st.chat_input(placeholder="Say something to our AI Agent...",
+                    max_chars=1500, # I do not have infinite budget for tokens
                     accept_file=True,
                     file_type=[".png", ".jpg", ".jpeg"]):
     # prompt object: (text="{string}", files=[])
 
     # user query
     if prompt.text:
-        st.chat_message("role").markdown(prompt.text)
-        st.session_state.messages.append({"role": "user", "content": prompt.text})
-    if prompt["files"]:
-        st.chat_message("role").image(prompt["files"][0])
+        with st.chat_message("user"):
+            st.markdown(prompt.text)
+            if prompt["files"]:
+                st.image(prompt["files"][0])
+            st.session_state.messages.append({"role": "user", "content": prompt})
 
     # AI response
-    response = f"{prompt.text} (TODO: GET ACTUAL AI RESPONSE)" # TODO: get response from AI in ./app.py
+    print("[DEBUG] ", prompt)
+    response = app.get_response(prompt.text)
 
     st.chat_message("assistant").markdown(response)
     st.session_state.messages.append({"role": "assistant", "content": response})
